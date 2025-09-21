e4056311be4d3444000760f22cc9ed72f1daaacd
xiyaoeva <xiyaolee@outlook.com>
2025-09-21 13:06:05 +0800

    Add files via upload

---
+++ gateway-service/src/main/java/io/smartcommerce/gateway-service/Application.java
@@ -0,0 +1,19 @@
+
+package io.smartcommerce.gateway-service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    @GetMapping("/health")
+    public String health() {
+        return "gateway-service is up";
+    }
+}
